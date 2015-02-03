cd0b5c672225bb39f3d5cfc1889f6298ffcf2159
Darren Garvey <darren.garvey@youview.com>
2015-02-03 00:26:20 +0000

    Add setup-machine.sh script.
    This script is intended to be the only one you need to run.
    
    It should be re-entrant. That means that every script
    it runs should be re-entrant.
    
    It simply runs every script under ./setup.d/ in lexicographical
    order.

---
+++ setup-machine.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+this_dir=$(dirname $(readlink -f $BASH_SOURCE))
+
+# Run all of the executable scripts in ./setup.d/
+# NB. This script guarantees the scripts are run from the directory that
+#     *this* script is in. (ie. $this_dir)
+cd ${this_dir}
+
+setup_dir="${this_dir}/setup.d"
+if [[ -d ${setup_dir} && -r ${setup_dir} && -x ${setup_dir} ]]; then
+    for i in $(LC_ALL=C command ls ${setup_dir}); do
+        if [[ -x ${setup_dir}/$i ]]; then
+            printf "Running \"%s\"..." $(basename ${setup_dir}/$i)
+            ${setup_dir}/$i && printf " DONE\n"
+        fi
+    done
+fi
+
